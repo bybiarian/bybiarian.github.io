0adf0dddc51489bf85cee9446a55ec7b94f9a9b7
bybiarian <a@byarian.dev>
2024-06-17 01:45:12 -0400

    more tweaks to animation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './styles.scss';
-
-// Check if platform is iOS to disable rainbow animation
-function isIOS() {
-    const userAgent = window.navigator.userAgent.toLowerCase();
-    return /iphone|ipad|ipod/.test(userAgent);
-  }
-
-  // Check if the current platform is iOS and add class to disable animation
-  if (isIOS()) {
-    document.body.classList.add('disable-animation');
-  }
